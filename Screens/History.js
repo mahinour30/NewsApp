0823bffa97fec1bdb42be48827a3b877706f59a0
mahinour30 <mahinour1233@gmail.com>
2020-12-02 23:36:46 +0200

    Create History.js

---
+++ Screens/History.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect}  from 'react';
+import {View, Text, Button} from 'react-native';
+
+const History =({navigation})=>{
+
+  return(
+    <View>
+        <Button
+        title='History'/>
+    </View>
+  )
+}
+
+export default History;
