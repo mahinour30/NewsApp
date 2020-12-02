adcc01c16552511b64c958e4663223cc86ed9c01
mahinour30 <mahinour1233@gmail.com>
2020-12-02 23:36:40 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Posts from './Screens/Posts';
+import Sources from './Screens/Sources';
+import History from './Screens/History';
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App =()=>{
   return(
-   <View>
-     <Text>
-       HELLO!
-     </Text>
-   </View>
+    <NavigationContainer>
+   <Tab.Navigator>
+    <Tab.Screen name="Posts" component={Posts} />
+    <Tab.Screen name="Sources" component={Sources} />
+    <Tab.Screen name="History" component={History} />
+   </Tab.Navigator>
+
+
+    </NavigationContainer> 
   );
 }
 
