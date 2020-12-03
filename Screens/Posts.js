740231f47d5740067e6cad628838e63fc8bbc6ff
mahinour30 <mahinour1233@gmail.com>
2020-12-03 06:08:49 +0200

    Update Posts.js

--- Screens/Posts.js
+++ Screens/Posts.js
@@ -1,25 +1,80 @@
 import React, {useState, useEffect}  from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet, ScrollView} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet, ScrollView, Alert} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const PostsStack = createStackNavigator();
+
+const URL = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=378c8ec60f864f2f839fd9499e69b4ce';
 const Posts =({navigation})=>{
 
+    const [isLoading, setLoading]=useState(true);
+    const [data, setData]=useState([]);
+
+
+
+    useEffect(()=>{
+        fetch(URL)
+        .then((response)=> response.json())
+        .then((json)=> {
+          setData(json.articles)
+        })
+        .catch((error) => Alert.alert('Error','Something IS Wrong!'))
+        .finally(()=>{setLoading(false)
+        });
+      }, []);
+
+
+
   return(
+    
+
     <ScrollView>
-        <View style ={styles.sliderContainer}>
-       <TouchableOpacity onPress={()=>{navigation.navigate('SinglePost')}}>
-    <View style = {styles.card}>
-    <View style={styles.cardImgWrapper}>
-      <Image  source={require('./Cookies.jpg')} 
-      resizeMode='cover' style={styles.cardImg}/> 
-    </View>
-    <View style = {styles.cardInfo}>
-      <Text style={styles.cardTitle}>Title</Text>
-      <Text style= {styles.cardDetails}>Details</Text>
-    </View>
-    </View>
-    </TouchableOpacity>
-    </View>
+ {
+     data != null &&
+
+<FlatList style={{flex:1}}
+//contentContainerStyle={{alignItems:"center"}}
+ data={data}
+ keyExtractor={({id}, index)=>id}
+ renderItem={({item})=>{
+    return(
+        <View style ={styles.sliderContainer}> 
+        <TouchableOpacity onPress={()=>navigation.navigate("SinglePost", {
+            postIMG:item.urlToImage,
+            postTitle:item.title,
+            PostAuthor:item.author,
+            PostDate:item.publishedAt,
+            PostContent:item.content,
+            postSource:item.source.name,
+            PostURL:item.url,
+          })}>
+     <View style = {styles.card}>
+     <View style={styles.cardImgWrapper}>
+       <Image   source={{ uri: item.urlToImage }} 
+       resizeMode='cover' style={styles.cardImg}/> 
+     </View>
+     <View style = {styles.cardInfo}>
+    <Text style={styles.cardTitle}>{item.author}</Text>
+       <Text style= {styles.cardDetails}>{item.title}</Text>
+       <Text style= {styles.time}>{item.publishedAt}</Text>
+
+     </View>
+     </View>
+     </TouchableOpacity>
+     </View>
+        )
+ }
+
+ }
+/>
+
+
+
+ }
+
     </ScrollView>
+
   )
 }
 
@@ -35,16 +90,15 @@ const styles= StyleSheet.create({
     sliderContainer:{
       height:200,
       width:'90%',
-      marginTop:10,
+      marginTop:2,
       justifyContent:'center',
       alignSelf:'center',
       borderRadius:8,
     },
     card:{
       height:100,
-      marginVertical:10,
       flexDirection:'row',
-      shadowColor:'pink',
+      shadowColor:'gray',
       shadowOffset:{width:0, height:1},
       shadowOpacity:0.8,
       shadowRadius:2,
@@ -64,7 +118,7 @@ const styles= StyleSheet.create({
     cardInfo:{
       flex:2,
       padding:10,
-      borderColor:'pink',
+      borderColor:'gray',
       borderWidth:1,
       borderLeftWidth:0,
       borderBottomRightRadius:8,
@@ -77,5 +131,10 @@ const styles= StyleSheet.create({
     cardDetails:{
       fontSize:12,
       color:'grey',
+    },
+    time:{
+        fontSize:10,
+        color:'grey',
+        alignItems:'flex-end'
     },
 });
