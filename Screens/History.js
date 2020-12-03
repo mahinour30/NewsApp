11f614a68124be874a4c3cc42e47de12321d64f4
mahinour30 <mahinour1233@gmail.com>
2020-12-03 21:45:11 +0200

    Update History.js

--- Screens/History.js
+++ Screens/History.js
@@ -1,11 +1,22 @@
 import React, {useState, useEffect}  from 'react';
 import {View, Text, Button} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const History =({navigation})=>{
+
+
+  const getData = async()=>{
+    const userDocument = await firestore()
+    .collection('Posts')
+    .doc('UJQj1J51IafE3u75eU9k')
+    .get()
+    console.log(userDocument)
+  }
 
   return(
     <View>
         <Button
+        onPress={()=>{getData()}}
         title='History'/>
     </View>
   )
